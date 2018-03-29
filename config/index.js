1c9dedbbf28628ec2ebd5c43091be2f6027a9c4f
juezhan <11991762@qq.com>
2018-03-29 09:46:11 +0800

    第一次提交

---
+++ config/index.js
@@ -0,0 +1,37 @@
+'use strict'
+// Template version: 1.3.1
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+
+module.exports = {
+  dev: {
+
+    // Paths
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/',
+    proxyTable: {},
+    host: 'localhost', // can be overwritten by process.env.HOST
+    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    autoOpenBrowser: false,
+    errorOverlay: true,
+    notifyOnErrors: true,
+    poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
+    useEslint: true,
+    showEslintErrorsInOverlay: false,
+    devtool: 'cheap-module-eval-source-map',
+    cacheBusting: true,
+    cssSourceMap: true
+  },
+
+  build: {
+    env: require('./prod.env'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsSubDirectory: '/',
+    assetsPublicPath: '/',
+    productionSourceMap: true,
+    productionGzip: false,
+    productionGzipExtensions: ['js', 'css'],
+    bundleAnalyzerReport: process.env.npm_config_report
+  }
+}
---
+++ src/components/Welcome.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  div.welcome
+    h1 Welcome
+</template>
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+<style scoped lang="stylus" type="text/stylus">
+  .welcome
+    h1
+      color #00ff00
+</style>
